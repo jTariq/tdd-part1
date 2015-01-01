7856d30f08746f43a3623f8aabc86537b92184e3
jTariq <jTariq@users.noreply.github.com>
2015-01-01 12:13:13 +0500

    Refactoring
    Removing unused overrides for now

--- Assignment0.java
+++ Assignment0.java
@@ -31,21 +31,4 @@ public class Assignment0 {
 		assertEquals(new Franc(15), five.times(3));
 	}
 	
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
-
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-	}
-
-	@Before
-	public void setUp() throws Exception {
-	}
-
-	@After
-	public void tearDown() throws Exception {
-	}
-
-
 }
