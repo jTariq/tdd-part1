8bd3bbb461149e0543c3626f6439811f57822847
jTariq <jTariq@users.noreply.github.com>
2015-01-01 22:10:38 +0500

    Chapter 9
    Chapter 9

--- Assignment0.java
+++ Assignment0.java
@@ -31,4 +31,9 @@ public class Assignment0 {
 		assertEquals(Money.franc(15), five.times(3));
 	}
 	
+	@Test public void testCurrency() {
+		assertEquals("USD", Money.dollar(1).currency());
+		assertEquals("CHF", Money.franc(1).currency());
+	}
+	
 }
