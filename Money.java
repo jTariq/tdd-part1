0cc5919e06e4e6cf659c439dfc878785d32e9cdc
jTariq <jTariq@users.noreply.github.com>
2015-01-01 15:35:55 +0500

    Chapter 8
    Chapter 8

--- Money.java
+++ Money.java
@@ -1,6 +1,15 @@
-class Money {
+abstract class Money {
+	abstract Money times(int multiplier);
 
 	protected int amount;
+	
+	static Money dollar(int amount) {
+		return new Dollar(amount);
+	}
+
+	static Money franc(int amount) {
+		return new Franc(amount);
+	}
 
 	public boolean equals(Object object) {
 		Money money = (Money) object;
