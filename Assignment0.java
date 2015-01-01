068a5561172f90874d8cffbb63e2d5014cf182b6
jTariq <jTariq@users.noreply.github.com>
2015-01-01 11:19:00 +0500

    Chapter 5
    Chapter 5

--- Assignment0.java
+++ Assignment0.java
@@ -22,6 +22,12 @@ public class Assignment0 {
 		assertFalse(new Dollar(5).equals(new Dollar(6)));
 	}
 
+	@Test public void testFrancMultiplication() {
+		Franc five= new Franc(5);
+		assertEquals(new Franc(10), five.times(2));
+		assertEquals(new Franc(15), five.times(3));
+	}
+	
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 	}
